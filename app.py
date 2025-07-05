d5371c2dba2d6e2c7448eaacd622f772ec3cac41
onkarshinde77 <shindeonkar707@gmail.com>
2025-07-05 08:49:00 +0530

    Improve helper file code

--- app.py
+++ app.py
@@ -4,24 +4,28 @@ from preprocess import preprocess
 from helper import fetch_stats
 
 st.sidebar.title("Whatsapp chat Analyzer")
-uploaded_file = st.sidebar.file_uploader("Choose a file")
+# uploaded_file = st.sidebar.file_uploader("Choose a file")
+uploaded_file = open('chat.txt','r',encoding='utf-8').read()
 
 if uploaded_file is not None:
-    byte_data = uploaded_file.getvalue()
-    data = byte_data.decode('utf-8')
+    # byte_data = uploaded_file.getvalue()
+    # data = byte_data.decode('utf-8')
+    data = uploaded_file
     data = preprocess(data)
     st.dataframe(data)
     
     user_list = data['sender'].unique()
     user_list.sort()
-    np.insert(user_list,0,"All Users")
-    
-    selected_user = st.sidebar.selectbox("users",user_list) 
+    user_list = np.insert(user_list,0,"All Users")
+    selected_user = st.sidebar.selectbox("users",user_list)
     
     if st.sidebar.button("Show Analysis"):
         
-        num_message = fetch_stats(selected_user,data)
-        col1,col2,col3,col4 = st.beta_columns(4)
+        num_message,words = fetch_stats(selected_user,data)
+        col1,col2,col3,col4 = st.columns(4)
         with col1:
             st.header("Total Messages")
             st.title(num_message)
+        with col2:
+            st.header("Total Words")
+            st.title(len(words))
--- helper.py
+++ helper.py
@@ -1,5 +1,10 @@
 
-
 def fetch_stats(selected_user,data):
-    if selected_user == 'All Users':
-        return data.shape[0]
+    new_df = data
+    if selected_user != 'All Users':
+        new_df = data[data['sender'] == selected_user ]
+    words=[]
+    num = new_df.shape[0]
+    for mess in new_df['messages']:
+        words.extend(mess.split())
+    return num,words
