593062288f6c73cc8f164f105a06b6aeeb7b02dd
onkarshinde77 <shindeonkar707@gmail.com>
2025-07-06 19:31:08 +0530

    find max word in message login build

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import streamlit as st
 import numpy as np
 from preprocess import preprocess
-from helper import fetch_stats
+from helper import fetch_stats , active_user , create_wordcloud,count_max_word
+import matplotlib.pyplot as plt
 
 st.sidebar.title("Whatsapp chat Analyzer")
 # uploaded_file = st.sidebar.file_uploader("Choose a file")
-uploaded_file = open('chat.txt','r',encoding='utf-8').read()
+uploaded_file = open('chats/chat2.txt','r',encoding='utf-8').read()
 
 if uploaded_file is not None:
     # byte_data = uploaded_file.getvalue()
@@ -21,7 +22,7 @@ if uploaded_file is not None:
     
     if st.sidebar.button("Show Analysis"):
         
-        num_message,words = fetch_stats(selected_user,data)
+        num_message,words,media_count,link_count = fetch_stats(selected_user,data)
         col1,col2,col3,col4 = st.columns(4)
         with col1:
             st.header("Total Messages")
@@ -29,3 +30,41 @@ if uploaded_file is not None:
         with col2:
             st.header("Total Words")
             st.title(len(words))
+        with col3:
+            st.header("Total Media")
+            st.header(media_count)
+        with col4:
+            st.header("Total Links")
+            st.title(link_count)
+            
+        if selected_user == "All Users":
+            st.title("Most Active Users")
+            X,active_percentage = active_user(data)
+            fig , ax = plt.subplots()
+            col1,col2 = st.columns(2)
+            with col1:
+                ax.bar(X.index,X.values,color='red')
+                plt.xticks(rotation='vertical')
+                st.pyplot(fig)
+            with col2:
+                st.dataframe(active_percentage)
+        # wordcloud
+        df_wc = create_wordcloud(selected_user,data)
+        fig ,ax = plt.subplots()
+        st.title("Most use words")
+        ax.imshow(df_wc)
+        st.pyplot(fig)
+        
+        # max word
+        st.title("Most used word")
+        col1,col2 = st.columns(2)
+        max_word = count_max_word(selected_user,data)
+        with col1: 
+            st.write(max_word.head(10))
+            # st.write(max_word.shape)
+        with col2:
+            fig ,ax = plt.subplots()
+            ax.barh(max_word['word'].head(10),max_word['count'].head(10))
+            plt.xticks(rotation='vertical')
+            st.pyplot(fig)
+
--- helper.py
+++ helper.py
@@ -1,3 +1,6 @@
+from wordcloud import WordCloud
+import pandas as pd
+from collections import Counter
 
 def fetch_stats(selected_user,data):
     new_df = data
@@ -7,4 +10,89 @@ def fetch_stats(selected_user,data):
     num = new_df.shape[0]
     for mess in new_df['messages']:
         words.extend(mess.split())
-    return num,words
+    # count media
+    media = new_df[new_df['messages']=='<Media omitted>'].shape[0]
+    # count links
+    link_count = new_df[new_df['messages'].str.startswith("https://") | new_df['messages'].str.startswith("www.")].shape[0]
+
+    return num,words,media, link_count
+
+
+def active_user(data):
+    active_users = data['sender'].value_counts()
+    per = round((data['sender'].value_counts()/data.shape[0])*100,2).reset_index()
+    per = per.rename(columns={'index':"name",'sender':'percentage'})
+    per['percentage'] = per['percentage'].astype(str) + '%'
+    return active_users,per
+
+def create_wordcloud(selected_user,data):
+    df = data
+    if selected_user != 'All Users':
+        df = data[data['sender'] == selected_user]
+    wc = WordCloud(width=300,height=300,min_font_size=10,background_color='white')
+    df_wc = wc.generate(df['messages'].str.cat(sep=" ")) 
+    return df_wc
+
+# ------------------------------------- Fuctions ------------------------------------------------------------
+
+def count_word(word,data):
+    count=0
+    for i in data:
+        if word== i:
+            count+=1
+    return count
+
+def sort_(words):
+    words=list(words)
+    n = len(words)
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if words[j][1] < words[j + 1][1]:
+                words[j], words[j + 1] = words[j + 1], words[j]
+    return words
+
+def clean_word(str):
+    char = ['!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/',':', ';', 
+            '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~']
+    str = str.lower().strip()
+    st=""
+    for i in str:
+        if i in char:
+            continue
+        st+=i
+    return st
+        
+# ------------------------------------------------------------------------------------------------------------
+
+def count_max_word(selected_user,data):
+    new_df = data
+    new_df = new_df[new_df['messages'] != '<Media omitted>' ]
+    if selected_user != 'All Users':
+        new_df = data[data['sender'] == selected_user ]
+    
+    stop_word = ["i","me","my","myself","we","our","ours","ourselves","you","your","yours","yourself","yourselves","he","him","his","himself","she","her","hers","herself","it","its","itself","they","them","their","theirs","themselves","what","which","who","whom","this","that","these","those","am","is","are","was","were","be","been","being","have","has","had","having","do","does","did","doing","a","an","the","and","but",
+                 "if","or","because","as","until","while","of","at","by","for","with","about","against","between","into","through","during","before","after","above","below","to","from","up","down","in","out","on","off","over","under","again","further","then","once","here","there","when","where","why","how","all","any","both","each","few","more","most","other","some","such","no","nor","not","only","own","same","so","than",
+                 "too","very","s","t","can","will","just","don","should","now","main", "mujhe", "mera", "meri", "hum", "hamara", "hamare", "tum", "tumhara", "tumhare","vah", "wo", "yah", "ye", "jo", "kaun", "kya", "kaise", "kahan", "kab", "kyon","hai", "hain", "tha", "the", "tha", "tha", "tha", "tha", "tha", "tha","aur", "lekin", "ya", "kyonki", "to", "yadi", "jab", "tak", "se", "par", "mein", "ka", "ki", "ke",
+                "bhi", "na", "nahi", "is", "us", "un", "in", "unke", "apne", "apne", "apne","karna", "kiya", "kiye", "karte", "karta", "karti", "kar", "raha", "rahi", "rahe","hona", "hua", "hui", "hue", "hain", "hai", "tha", "the", "tha", "the","yahan", "wahan", "kahan", "kis", "kisi", "kuch", "sab", "sabhi", "kuch", "koi","1","2","3","4","5","6","7","8","9","0",'!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/',':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~']
+
+    words = []
+    for mess in new_df['messages']:
+        words.extend(mess.split())
+
+    
+    # # Filter out stop words
+    filtered_words = [word for word in words if word.lower().strip() not in stop_word]
+    
+    # clean word(exclude * ' " ect )
+    words = [clean_word(word) for word in filtered_words]
+    # count words
+    word_counts = Counter(words)
+    
+    # # Convert to list of (word, count) tuples
+    max_repeat_word = [list(item) for item in word_counts.items()]
+
+    # # Sort using your manual sort
+    temp = sort_(max_repeat_word)
+    temp = pd.DataFrame(temp, columns=['word', 'count'])
+    temp = temp[temp['word'] != ""]
+    return temp
